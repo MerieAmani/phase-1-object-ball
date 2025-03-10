28cc462db15b53a5918feb0a92e620cd2aea826f
matthew <meriematthew01@gmail.com>
2025-03-10 15:04:21 +0300

    adds objectball

--- src/01-simple-debug.js
+++ src/01-simple-debug.js
@@ -4,3 +4,11 @@ debugger
 let x = 99
 debugger
 console.log(x)
+
+let obj = { a: 1, b: 2, c: 3 }
+
+console.log('Keys:', Object.keys(obj))
+
+console.log('Values:', Object.values(obj))
+
+console.log('Entries:', Object.entries(obj))
